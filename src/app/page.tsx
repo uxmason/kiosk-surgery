5063953c9774b4e6912176814677d3176949b8c4
uxmason <uxmason@me.com>
2025-02-19 15:05:51 +0900

    ..

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <>
       <main>
-        <div style={{background:'url(./images/image20240219.png) no-repeat', backgroundPosition:'center', backgroundSize:'cover'}}></div>
+        <div style={{background:'url(./images/image20240219.png) no-repeat', backgroundPosition:'center', backgroundSize:'cover', height:'1980px', width: '100%'}}></div>
       </main>
       <footer>
         
